bb76e4a2004a841b7f15aec170e7b097da0c3853
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-05-29 16:02:16 -0400

    3.6. Your First API / Why Models instead of Entities?

---
+++ TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>();
+
+        }
+    }
+}
